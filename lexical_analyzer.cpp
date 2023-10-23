baea63539bd858bf921c9dccc268262869e1d100
Franz <106358025+Franzdomingo@users.noreply.github.com>
2023-10-23 12:43:18 +0800

    Update lexical_analyzer.cpp

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -10,9 +10,15 @@ using namespace std;
 
 // Function to check if a token is a keyword
 bool isKeyword(const string& token) {
-    unordered_set<string> keywords = {"if", "else", "while", "int", "float", "return"};
+    unordered_set<string> keywords = {"int", "float", "return", "cout", "main", "using", "namespace", "std", "include", "iostream", "auto", "break", "case", "char", "const", "continue",
+        "default", "do", "double", "else", "enum", "extern",
+        "float", "for", "goto", "if", "int", "long", "register",
+        "return", "short", "signed", "sizeof", "static", "struct",
+        "switch", "typedef", "union", "unsigned", "void", "volatile",
+        "while"};
     return keywords.count(token) > 0;
 }
+
 
 int main() {
     string filename = "sourceFile.txt";
@@ -37,26 +43,31 @@ int main() {
         while (inputStream >> token) {
             if (isKeyword(token)) {
                 keywordsCount++;
+                cout << " " << token << " is a keyword" << endl;
             } else if (isalpha(token[0]) || token[0] == '_') {
                 identifiersCount++;
-            } else if (token == "+" || token == "-" || token == "*" || token == "/") {
+                cout << " " << token << " is an identifier" << endl;
+            } else if (token == "+" || token == "-" || token == "=" || token == "/" || token == "<<") {
                 operatorsCount++;
+                cout << " " << token << " is an operator" << endl;
             } else if (ispunct(token[0])) {
                 symbolsCount++;
+                cout << " " << token << " is a symbol" << endl;
             } else if (isdigit(token[0])) {
                 constantsCount++;
+                cout << " " << token << " is a constant" << endl;
             }
+                cout << "—-SUMMARY—-" << endl;
+                cout << "Keywords: " << keywordsCount << endl;
+                cout << "Identifiers: " << identifiersCount << endl;
+                cout << "Operators: " << operatorsCount << endl;
+                cout << "Symbols: " << symbolsCount << endl;
+                cout << "Constants: " << constantsCount << endl;
+
         }
     }
 
     inputFile.close();
-
-    cout << "—-SUMMARY—-" << endl;
-    cout << "Keywords: " << keywordsCount << endl;
-    cout << "Identifiers: " << identifiersCount << endl;
-    cout << "Operators: " << operatorsCount << endl;
-    cout << "Symbols: " << symbolsCount << endl;
-    cout << "Constants: " << constantsCount << endl;
 
     return 0;
 }
